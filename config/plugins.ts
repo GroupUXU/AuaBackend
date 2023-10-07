a39dad607498fe6aa58e72c2bb02383916a9d70d
Paweł Niedźwiecki <hello@uxu.pl>
2023-10-07 19:59:51 +0200

    firest commit

---
+++ config/plugins.ts
@@ -0,0 +1,48 @@
+module.exports = ({env}) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("DEFAULT_EMAIL"),
+        defaultReplyTo: env("DEFAULT_EMAIL"),
+        testAddress: env("DEFAULT_EMAIL"),
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+    },
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 500,
+      amountLimit: 1000,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+  meilisearch: {
+    config: {
+      // Your meili host
+      host: "https://ms-f01677209658-5386.fra.meilisearch.io",
+      // Your master key or private key
+      apiKey: env("MEILI_SEARCH"),
+    }
+  }
+});
